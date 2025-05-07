debdf5dffd61f9ab00d9d03b01fb141a8554eba1
Brem0317 <patrickbarcelo17@gmail.com>
2025-05-07 20:35:21 +0800

    remove delete

--- app/Filament/Resources/FoodTransactionResource/Pages/EditFoodTransaction.php
+++ app/Filament/Resources/FoodTransactionResource/Pages/EditFoodTransaction.php
@@ -13,7 +13,7 @@ class EditFoodTransaction extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            // Actions\DeleteAction::make(),
         ];
     }
 }
